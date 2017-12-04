c7beb388a5527eb7b84d2a7f84b55675f5bea226
lwc <wence.lv@cnlaunch.com>
2017-12-04 18:17:21 +0800

    构建1

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div id="app">
-    
+    <!-- <m-header><m-header> -->
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
+import MHeader from "./components/m-header/m-header";
 export default {
   name: "app",
-  components: {}
+  components: {
+    MHeader
+  }
 };
 </script>
 
-<style>
-
+<style scoped lang="stylus" rel="stylesheet/stylus">
 </style>
---
+++ src/components/m-header/m-header.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="m-header">
+    <h1 class="text">区块链浏览器</h1>
+  </div>
+</template>
+
+
+<script type="text/ecmascript-6">
+export default {};
+</script>
+
+// <style scoped lang="stylus" rel="stylesheet/stylus">
+</style>
