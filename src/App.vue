fdc9e648e1bc8bc918238b1a19a4859a49f9ee54
吕文策 <23797610@qq.com>
2017-12-05 23:22:03 +0800

    home结构

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template>
   <div id="app">
     <h1 class="text">区块链浏览器</h1>
-    <router-link to="/home">首页</router-link>
-    <router-link to="/block">区块公示</router-link>
-    <router-link to="/card">存证公示</router-link>
-    <router-link to="/account">账户身份</router-link>
+    <div class="nav">
+      <router-link to="/home">首页</router-link>
+    </div>
+    <div class="nav">
+      <router-link to="/block">区块公示</router-link>
+    </div>
+    <div class="nav">
+      <router-link to="/card">存证公示</router-link>
+    </div>
+    <div class="nav">
+      <router-link to="/account">账户身份</router-link>
+    </div>
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
@@ -13,6 +21,7 @@
 
 <script type="text/ecmascript-6">
 import axios from "axios";
+import "./common/stylus/index.styl";
 export default {
   name: "app",
   data() {
@@ -34,6 +43,10 @@ export default {
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
 h1 {
+  display: inline-block;
+}
+
+.nav {
   display: inline-block;
 }
 </style>
--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,6 +1,55 @@
 <template>
-  <div class="m-header">
-    home
+  <div class="home">
+    <div class="">
+      <input type="text" >
+      <button>搜索</button>
+    </div>
+
+    <div class="">
+      <ul class="">
+        <li>
+          当前区块高度<span>258837</span>
+        </li>
+        <li>
+          记帐节点数<span>4</span>
+        </li>
+        <li>
+          存证TX条目数量<span>2</span>
+        </li>
+      </ul>
+    </div>
+
+    <div class="">
+      <div class="">
+        <h2>最新区块</h2>
+        <span class="">查看全部</span>
+      </div>
+      <div class="">
+        <table class=''>
+          <thead>
+            <tr>
+              <th>区块高度</th>
+              <th>出块时间</th>
+              <th>出块节点名称</th>
+              <th>存证TX条目数量</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1111111111111111111</td>
+              <td>2222222222222222222</td>
+              <td>3333333333333333333</td>
+              <td>4444444444444444444</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <div class="">
+      <p>2017 元征区块链技术研究院.京ICP备88888888号-8</p>
+    </div>
+    
   </div>
 </template>
 
