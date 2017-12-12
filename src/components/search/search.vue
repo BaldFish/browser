a49999acd5a2bd9f814152caaf2f2f306fb5a73f
lwc <wence.lv@cnlaunch.com>
2017-12-12 18:13:29 +0800

    搜索获取数据

--- src/components/search/search.vue
+++ src/components/search/search.vue
@@ -7,14 +7,14 @@
         <option value="block_hash">区块哈希</option>
         <option value="trade_hash">交易哈希</option>
       </select>
-      <input class="search_ipt" type="text" placeholder="请输入查询条件">
-      <button class="btn">搜索</button>
+      <input class="search_ipt" type="text" placeholder="请输入查询条件" v-model="search_content">
+      <button class="btn" @click.prevent="search">搜索</button>
     </div>
 
     <div class="content">
       <table>
         <caption>
-          <p>查询时间：2017年12月12日 13:30:46</p>
+          <p>查询时间：2017-12-12 13:30:46</p>
         </caption>
         <colgroup>
           <col class="col1">
@@ -32,7 +32,7 @@
           <td></td>
         </tr>
         <tr>
-          <td>高度</td>
+          <td>高度{{search_content}}</td>
           <td></td>
         </tr>
         <tr>
@@ -58,7 +58,62 @@
 
 
 <script type="text/ecmascript-6">
-export default {};
+import _ from "lodash";
+export default {
+  props: {
+    apidata: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      search_content: "",
+      getNewBlock: {}
+    };
+  },
+  methods: {
+    search() {
+      console.log(this.search_content);
+      this.getNewBlock = _.find(this.apidata.getNewBlock, item => {
+        return item.blockNumber === this.search_content;
+      });
+      console.log(this.getNewBlock);
+    },
+    getFormatDate(val) {
+      var nowDate = new Date(val);
+      var year = nowDate.getFullYear();
+      var month =
+        nowDate.getMonth() + 1 < 10
+          ? "0" + (nowDate.getMonth() + 1)
+          : nowDate.getMonth() + 1;
+      var date =
+        nowDate.getDate() < 10 ? "0" + nowDate.getDate() : nowDate.getDate();
+      var hour =
+        nowDate.getHours() < 10 ? "0" + nowDate.getHours() : nowDate.getHours();
+      var minute =
+        nowDate.getMinutes() < 10
+          ? "0" + nowDate.getMinutes()
+          : nowDate.getMinutes();
+      var second =
+        nowDate.getSeconds() < 10
+          ? "0" + nowDate.getSeconds()
+          : nowDate.getSeconds();
+      return (
+        year +
+        "-" +
+        month +
+        "-" +
+        date +
+        " " +
+        hour +
+        ":" +
+        minute +
+        ":" +
+        second
+      );
+    }
+  }
+};
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
